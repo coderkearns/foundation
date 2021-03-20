530e9cc6e984a3e49c1f0879f6e59be94d612685
coderkearns <coder.kearns@gmail.com>
2021-03-20 18:42:27 -0500

    Update 'create' command, make 'create' default

--- util/create.js
+++ util/create.js
@@ -1,5 +1,7 @@
 const path = require("path")
+const fs = require('fs');
 const fse = require('fs-extra')
+const { exec } = require('shelljs');
 
 const vars = require("../vars.js")
 const templatepath = `${vars.dirname}/foundations`
@@ -7,5 +9,11 @@ const templatepath = `${vars.dirname}/foundations`
 module.exports = (foundation, name) => {
   sourcepath = path.resolve(name)
   fse.copySync(`${templatepath}/${foundation}`, sourcepath)
+  process.cwd(`./${name}/`)
+  if (fs.existsSync("./init.sh")) {
+    exec(`./init.sh ${name} ${sourcepath}`)
+    exec(`rm -rf ./init.sh`)
+  }
+  process.cwd(`../`)
   console.log(`Succesfully created folder ${name} from ${foundation} foundation.`)
 }
